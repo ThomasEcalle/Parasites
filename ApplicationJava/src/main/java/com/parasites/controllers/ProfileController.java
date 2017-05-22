fa36f9866c3d450c557d48ba5b1b63f3378f9f4f
Jean <spyroo913@hotmail.com>
2017-05-22 17:13:12 +0200

    ProfilController structure part 2

--- ApplicationJava/src/main/java/com/parasites/controllers/ProfileController.java
+++ ApplicationJava/src/main/java/com/parasites/controllers/ProfileController.java
@@ -5,13 +5,7 @@ package com.parasites.controllers;
  */
 public class ProfileController {
 
-    private String password;
-    private String password_confirmation;
-    private String mail;
-    private String mail_confirmation;
-    private String firstname;
-    private String lastname;
-    private String phone;
+    private String message;
 
     public ProfileController(String password,
                              String password_confirmation,
@@ -20,52 +14,35 @@ public class ProfileController {
                              String firstname,
                              String lastname,
                              String phone){
-        this.password = password;
-        this.password_confirmation = password_confirmation;
-        this.mail = mail;
-        this.mail_confirmation = mail_confirmation;
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.phone = phone;
+        message += changePassword(password, password_confirmation);
+        message += changeMail(mail, mail_confirmation);
+        message += changeFirstname(firstname);
+        message += changeLastname(lastname);
+        message += changePhone(phone);
     }
 
-    public String displayAPICalls(){
-        String message = "";
-        if(password != "") {
-            message += changePassword();
-        }
-        if(mail != "") {
-            message += ("\n" + changeMail());
-        }
-        if(firstname != "") {
-            message += ("\n" + changeFirstname());
-        }
-        if(lastname != "") {
-            message += ("\n" + changeLastname());
-        }
-        if(phone != "") {
-            message += ("\n" + changePhone());
-        }
-        return message;
+    public String changePassword(String password, String password_confirmation){
+        if(password == "") return "";
+        return "";
     }
 
-    public String changePassword(){
-        return "ok";
+    public String changeMail(String mail, String mail_confirmation){
+        if(mail == "") return "";
+        return "";
     }
 
-    public String changeMail(){
-        return "ok";
+    public String changeFirstname(String firstname){
+        if(firstname == "") return "";
+        return "";
     }
 
-    public String changeFirstname(){
-        return "ok";
+    public String changeLastname(String lastname){
+        if(lastname == "") return "";
+        return "";
     }
 
-    public String changeLastname(){
-        return "ok";
-    }
-
-    public String changePhone(){
-        return "ok";
+    public String changePhone(String phone){
+        if(phone == "") return "";
+        return "";
     }
 }
